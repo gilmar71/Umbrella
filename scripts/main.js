5dc0c6f019c2a843ffc118a681c6419381270e36
gilmar71 <gil7ma@gmail.com>
2022-06-07 10:03:04 -0300

    feat: create umbrella.app

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $(".owl-dicas").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    items: 3,
+  });
+});
+$('.owl-banner').owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: false,
+  items: 1,
+  dots: true,
+});
