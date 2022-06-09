cd55cc8949329891681702dac1523be00c6da45d
gilmar71 <gil7ma@gmail.com>
2022-06-09 09:04:45 -0300

    feat: blockbar

--- scripts/main.js
+++ scripts/main.js
@@ -14,4 +14,11 @@
 //   dots: true,
 // });
 
-console.log("teste");
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu() {
+  const btnMobile = document.getElementById("nav");
+  nav.classList.toggle("active");
+}
+
+btnMobile.addEventListener("click", toggleMenu);
