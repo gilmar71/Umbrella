42ea4f68d69b4248b8436f0c90c3a80ac41b48a0
gilmar71 <gil7ma@gmail.com>
2022-06-09 08:01:56 -0300

    feat: limpeza

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,17 @@
-$(document).ready(function () {
-  $(".owl-dicas").owlCarousel({
-    loop: true,
-    margin: 20,
-    nav: false,
-    items: 3,
-  });
-});
-$('.owl-banner').owlCarousel({
-  loop: true,
-  margin: 0,
-  nav: false,
-  items: 1,
-  dots: true,
-});
+// $(document).ready(function () {
+//   $(".owl-dicas").owlCarousel({
+//     loop: true,
+//     margin: 20,
+//     nav: false,
+//     items: 3,
+//   });
+// });
+// $(".owl-banner").owlCarousel({
+//   loop: true,
+//   margin: 0,
+//   nav: false,
+//   items: 1,
+//   dots: true,
+// });
+
+console.log("teste");
